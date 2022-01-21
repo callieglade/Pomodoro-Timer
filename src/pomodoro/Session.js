60970d4ace57140dd4c468ee2c7c85244d2b1c8b
Callie Glade <callie.glade@meraki.net>
2022-01-21 15:22:09 -0800

    Update times to use seconds/minutes converter functions

--- src/pomodoro/Session.js
+++ src/pomodoro/Session.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { minutesToDuration, secondsToDuration } from "../utils/duration";
 
-function Session({ session }) {
+function Session({ session, focusDuration }) {
+  if (!session) return null;
   return (
     <div>
       {/* TODO: This area should show only when there is an active focus or break - i.e. the session is running or is paused */}
@@ -8,11 +10,11 @@ function Session({ session }) {
         <div className="col">
           {/* TODO: Update message below to include current session (Focusing or On Break) total duration */}
           <h2 data-testid="session-title">
-            {/* {session.label} for 25:00 minutes */}
+            {session.label} for {minutesToDuration(focusDuration)} minutes
           </h2>
           {/* TODO: Update message below correctly format the time remaining in the current session */}
           <p className="lead" data-testid="session-sub-title">
-            {/* {session.timeRemaining} remaining */}
+            {secondsToDuration(session.timeRemaining)} remaining
           </p>
         </div>
       </div>
