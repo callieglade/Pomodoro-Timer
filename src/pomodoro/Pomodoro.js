053d4fc58815f79e7c8bd9fab568b1b2f63cebaf
Callie Glade <callie.glade@meraki.net>
2022-01-20 16:46:33 -0800

    Replace Focus Duration JSX with FocusDuration component

--- src/pomodoro/Pomodoro.js
+++ src/pomodoro/Pomodoro.js
@@ -117,32 +117,10 @@ function Pomodoro() {
     <div className="pomodoro">
       <div className="row">
         <div className="col">
-          <div className="input-group input-group-lg mb-2">
-            <span className="input-group-text" data-testid="duration-focus">
-              {/* TODO: Update this text to display the current focus session duration */}
-              Focus Duration: {minutesToDuration(focusDuration)}
-            </span>
-            <div className="input-group-append">
-              {/* TODO: Implement decreasing focus duration and disable during a focus or break session */}
-              <button
-                type="button"
-                className="btn btn-secondary"
-                data-testid="decrease-focus"
-                onClick={changeFocusDuration}
-              >
-                <span className="oi oi-minus" />
-              </button>
-              {/* TODO: Implement increasing focus duration and disable during a focus or break session */}
-              <button
-                type="button"
-                className="btn btn-secondary"
-                data-testid="increase-focus"
-                onClick={changeFocusDuration}
-              >
-                <span className="oi oi-plus" />
-              </button>
-            </div>
-          </div>
+          <FocusDuration 
+            focusDuration = {focusDuration}
+            changeFocusDuration = {changeFocusDuration}
+          />
         </div>
         <div className="col">
           <div className="float-right">
