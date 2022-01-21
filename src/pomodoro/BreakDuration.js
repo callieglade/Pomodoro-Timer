acbfc85cafeb7da31e73172820cb8454634acd64
Callie Glade <callie.glade@meraki.net>
2022-01-20 18:23:30 -0800

    Move JSX for Break Duration to BreakDuration component

--- src/pomodoro/BreakDuration.js
+++ src/pomodoro/BreakDuration.js
@@ -1,7 +1,35 @@
-import React, { useState } from "react";
+import React from "react";
+import { minutesToDuration } from "../utils/duration";
 
-function BreakDuration() {
-
+function BreakDuration({ breakDuration, changeBreakDuration }) {
+    return (
+      <div className="input-group input-group-lg mb-2">
+        <span className="input-group-text" data-testid="duration-break">
+          {/* TODO: Update this text to display the current break session duration */}
+          Break Duration: {minutesToDuration(breakDuration)}
+        </span>
+        <div className="input-group-append">
+          {/* TODO: Implement decreasing break duration and disable during a focus or break session */}
+          <button
+            type="button"
+            className="btn btn-secondary"
+            data-testid="decrease-break"
+            onClick={changeBreakDuration}
+          >
+            <span className="oi oi-minus" data-testid="decrease-break" />
+          </button>
+          {/* TODO: Implement increasing break duration and disable during a focus or break session */}
+          <button
+            type="button"
+            className="btn btn-secondary"
+            data-testid="increase-break"
+            onClick={changeBreakDuration}
+          >
+            <span className="oi oi-plus" data-testid="increase-break" />
+          </button>
+        </div>
+      </div>
+    );
 }
 
 export default BreakDuration;
