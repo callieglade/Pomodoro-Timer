8058412fffb6f664dbe1098e3348ad6f04c0d737
Callie Glade <callie.glade@meraki.net>
2022-01-21 20:45:24 -0800

    Update classes to center PAUSED text on screen

--- src/pomodoro/Paused.js
+++ src/pomodoro/Paused.js
@@ -4,7 +4,9 @@ function Paused({ session, isTimerRunning }) {
   if (!session || isTimerRunning) return null;
   return (
     <div className="row mb-2">
-      <h2>Paused</h2>
+      <div className="col">
+        <h2>PAUSED</h2>
+      </div>
     </div>
   );
 }
